ba7c23c5dbb1cd7e3ba053ec1ec47fb4feb25394
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-24 20:45:18 +0300

    Enum
     Enum, это перечисления, которые позволяют разработчику определить набор именованных констант. TypeScript предоставляет как числовые, так и строковые перечисления
    Enum-ом в базовом понимании называют некий список возможных, заранее определенных, именованных значений. Нужен этот список для того, чтобы упростить работу разработчику в оперировании некими состояниями

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,76 +1,47 @@
-function add(n1:number, n2:number, showResult:boolean) {
-    // if(typeof n1 !== "nubmer" || typeof n2 !== "number"){
-    //     throw new Error("Incorrect input")
-    // }
-    return n1+n2
-}
-const number1 = "5"
-const nubmer2 = 2.8
-const printResult = true
-
-const result = add(+number1, nubmer2, printResult)
-console.log(result)
-
 // const person: {
 //   name: string;
 //   age: number;
 // } = {
-    const person1 = {
-        name: 'Maximilian',
-        age: 30
-      };
-      
-      console.log(person1.name);
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Maximilian',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
 
-      
-    // const person: {
-//   name: string;
-//   age: number;
-// } = {
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+
 const person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking']
-  };
-  
-  let favoriteActivities: string[];
-  favoriteActivities = ['Sports'];
-  
-  console.log(person.name);
-  
-  for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR !!!
-  }
-  
-  // const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person3: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-  } = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
-  };
-  
-  // person.role.push('admin');
-  // person.role[1] = 10;
-  
-  // person.role = [0, 'admin', 'user'];
-  
-  let favoriteActivities1: string[];
-  favoriteActivities = ['Sports'];
-  
-  console.log(person.name);
-  
-  for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR !!!
-  }
-  
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
+}
