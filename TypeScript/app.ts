4b17329a7db003fc7e798cf2c88cac3d4b4061bc
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-24 20:18:26 +0300

    типы тс
    Типы, тип обьект, тип массив

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,1 +1,45 @@
-console.log('Your code goes here...');
+function add(n1:number, n2:number, showResult:boolean) {
+    // if(typeof n1 !== "nubmer" || typeof n2 !== "number"){
+    //     throw new Error("Incorrect input")
+    // }
+    return n1+n2
+}
+const number1 = "5"
+const nubmer2 = 2.8
+const printResult = true
+
+const result = add(+number1, nubmer2, printResult)
+console.log(result)
+
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+    const person1 = {
+        name: 'Maximilian',
+        age: 30
+      };
+      
+      console.log(person1.name);
+
+      
+    // const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+  };
+  
+  let favoriteActivities: string[];
+  favoriteActivities = ['Sports'];
+  
+  console.log(person.name);
+  
+  for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+  }
+  
