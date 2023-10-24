9c3cf8fd16dbe57848130dd564355365be66da3d
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-24 20:30:28 +0300

    tuple
    tuple — это тип данных под названием кортежи, которые помогают решить эту задачу. Они представляют набор разнородных значений. Другими словами, кортежи обеспечивают хранение множественных полей различных типов.

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -43,3 +43,34 @@ const person = {
     // console.log(hobby.map()); // !!! ERROR !!!
   }
   
+  // const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person3: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+  } = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+  };
+  
+  // person.role.push('admin');
+  // person.role[1] = 10;
+  
+  // person.role = [0, 'admin', 'user'];
+  
+  let favoriteActivities1: string[];
+  favoriteActivities = ['Sports'];
+  
+  console.log(person.name);
+  
+  for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+  }
+  
