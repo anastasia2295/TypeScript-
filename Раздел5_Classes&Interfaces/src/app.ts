1e213f6b97c59b723d44bc8895e73fc302ad67be
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-27 09:00:46 +0300

    Функции конструктора и ключевое слово this

--- Раздел5_Classes&Interfaces/src/app.ts
+++ Раздел5_Classes&Interfaces/src/app.ts
@@ -4,5 +4,13 @@ class Department {
     constructor(n: string){
         this.name = n
     }
+
+    describe(this: Department) {
+        console.log("Department:" + this.name)
+    }
 }
 const accounting = new Department("Accounting")
+accounting.describe()
+
+const accountingCopy={ name:"DUMMY", describe: accounting.describe}
+accountingCopy.describe()
