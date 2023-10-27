526b3982a173dd9ca4381535d7237c2c88c7fdba
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-27 09:24:58 +0300

    Свойства «только для чтения»
    Модификатор readonly — это часть системы типов TypeScript . Он используется только компилятором для проверки незаконных присваивании значений.

--- Раздел5_Classes&Interfaces/src/app.js
+++ Раздел5_Classes&Interfaces/src/app.js
@@ -1,13 +1,21 @@
+
 "use strict";
 class Department {
-    constructor(n) {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        // private readonly id: string;
+        // private name: string;
         this.employees = [];
-        this.name = n;
+        // this.id = id;
+        // this.name = n;
     }
     describe() {
-        console.log("Department:" + this.name);
+        console.log(`Department (${this.id}): ${this.name}`);
     }
     addEmployee(employee) {
+        // validation etc
+        // this.id = 'd2';
         this.employees.push(employee);
     }
     printEmployeeInformation() {
@@ -15,10 +23,12 @@ class Department {
         console.log(this.employees);
     }
 }
-const accounting = new Department("Accounting");
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
+const accounting = new Department('d1', 'Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+// accounting.employees[2] = 'Anna';
 accounting.describe();
+accounting.name = 'NEW NAME';
 accounting.printEmployeeInformation();
-// const accountingCopy={ name:"DUMMY", describe: accounting.describe}
-// accountingCopy.describe()
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
