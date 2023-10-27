87a966faa65351d72d61ce07a845361af6edb5d7
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-27 09:20:08 +0300

    Сокращенная инициализация

--- Раздел5_Classes&Interfaces/src/app.ts
+++ Раздел5_Classes&Interfaces/src/app.ts
@@ -1,33 +1,39 @@
 class Department {
-    name: string;
-    private employees: string[]=[]
-
-    constructor(n: string){
-        this.name = n
+    // private id: string;
+    // private name: string;
+    private employees: string[] = [];
+  
+    constructor(private id: string, public name: string) {
+      // this.id = id;
+      // this.name = n;
     }
-
+  
     describe(this: Department) {
-        console.log("Department:" + this.name)
+      console.log(`Department (${this.id}): ${this.name}`);
     }
-
-    addEmployee(employee:string){
-        this.employees.push(employee)
+  
+    addEmployee(employee: string) {
+      // validation etc
+      this.employees.push(employee);
     }
-
-    printEmployeeInformation(){
-        console.log(this.employees.length)
-        console.log(this.employees)
-            
-        }
+  
+    printEmployeeInformation() {
+      console.log(this.employees.length);
+      console.log(this.employees);
     }
-
-const accounting = new Department("Accounting")
-
-accounting.addEmployee("Max")
-accounting.addEmployee("Manu")
-
-accounting.describe()
-accounting.printEmployeeInformation()
-
-// const accountingCopy={ name:"DUMMY", describe: accounting.describe}
-// accountingCopy.describe()
+  }
+  
+  const accounting = new Department('d1', 'Accounting');
+  
+  accounting.addEmployee('Max');
+  accounting.addEmployee('Manu');
+  
+  // accounting.employees[2] = 'Anna';
+  
+  accounting.describe();
+  accounting.name = 'NEW NAME';
+  accounting.printEmployeeInformation();
+  
+  // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+  
+  // accountingCopy.describe();
