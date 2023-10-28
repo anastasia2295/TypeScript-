39263acf1c8522972db26522be45d90753f0733e
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-28 18:30:24 +0300

    Модификатор protected
    Модификатор protected делает элемент доступным только для классов из того же пакета и всех подклассов. Это полезно, когда нужно предоставить доступ к определенным методам и переменным для наследников, но скрыть их от других классов

--- Раздел5_Classes&Interfaces/src/app.js
+++ Раздел5_Classes&Interfaces/src/app.js
@@ -33,6 +33,12 @@ class AccountingDepartment extends Department {
         super(id, 'Accounting');
         this.reports = reports;
     }
+    addEmployee(name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
     addReport(text) {
         this.reports.push(text);
     }
@@ -50,6 +56,9 @@ it.printEmployeeInformation();
 console.log(it);
 const accounting = new AccountingDepartment('d2', []);
 accounting.addReport('Something went wrong...');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 accounting.printReports();
+accounting.printEmployeeInformation();
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 // accountingCopy.describe();
