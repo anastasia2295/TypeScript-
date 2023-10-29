03467683de69016660b66bd7eec03f15608370ec
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-29 10:35:33 +0300

    Интерфейсы как типы функций

--- Раздел5_Classes&Interfaces/src/app.ts
+++ Раздел5_Classes&Interfaces/src/app.ts
@@ -1,4 +1,15 @@
-interface Named {
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+    (a: number, b: number): number;
+  }
+  
+  let add: AddFn;
+  
+  add = (n1: number, n2: number) => {
+    return n1 + n2;
+  };
+  
+  interface Named {
     readonly name: string;
   }
   
