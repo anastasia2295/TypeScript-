109b19266b5491ba9d8bd3f3732266d60e752751
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-31 22:18:41 +0300

    Ограничение "keyof"

--- Раздел7_Generics/src/app.ts
+++ Раздел7_Generics/src/app.ts
@@ -33,3 +33,13 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
   }
   
   console.log(countAndDescribe(['Sports', 'Cooking']));
+  
+  function extractAndConvert<T extends object, U extends keyof T>(
+    obj: T,
+    key: U
+  ) {
+    return 'Value: ' + obj[key];
+  }
+  
+  extractAndConvert({ name: 'Max' }, 'name');
+  
