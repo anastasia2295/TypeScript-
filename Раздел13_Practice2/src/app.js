440f196b99f833176228a595d4fc024bdda34ec0
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-11-08 21:04:08 +0300

    Рендеринг карты с помощью Google Maps

---
+++ Раздел13_Practice2/src/app.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const form = document.querySelector("form");
+const addressInput = document.getElementById("address");
+const GOOGLE_API_KEY = "AIzaSyCIaAc2c5M3VpbCH6PPq_guwy9lHuowXOs";
+function searchAddressHandler(event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    axios
+        .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+        .then(response => {
+        if (response.data.status !== "OK") {
+            throw new Error("Could not fetch location!");
+        }
+        const coordinates = response.data.results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById("map"), {
+            center: coordinates,
+            zoom: 16
+        });
+        new google.maps.Marker({ position: coordinates, map: map });
+    })
+        .catch(err => {
+        alert(err.message);
+        console.log(err);
+    });
+}
+form.addEventListener("submit", searchAddressHandler);
